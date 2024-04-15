e0092b5929ccf77eb08d101ec1aac246313f5ff1
Moosadeveloper <janmoosa77@gmail.com>
2024-04-15 21:47:36 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var num = "2" + "2";
+// Answer is 22 
+
+message = ("Hello," + "Dolly");
+alert(message);
+
+
+alert("33" + 3);
+// Answer is 333
+
+
+var a = "Pakistan";
+var b = "Zindabad";
+alert(a+b);
+
+var f = "The number is: " + 42;
+console.log(f);
+// with  +
+
+var m = "moosa";
+var n ="baloch"
+var l = m+n;
+console.log(l);
+
+
+
